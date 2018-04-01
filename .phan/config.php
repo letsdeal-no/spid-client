f4f1a41cd1d9acfbd2b8289a4604c4e490e86548
Maciej Kosiedowski <maciej.kosiedowski@schibsted.pl>
2018-04-01 23:20:52 +0200

    more quality checks

---
+++ .phan/config.php
@@ -0,0 +1,11 @@
+<?php
+return [
+   'target_php_version' => '7.1',
+   'directory_list' => [
+       'src',
+       'vendor',
+   ],
+   "exclude_analysis_directory_list" => [
+       'vendor/'
+   ],
+];
